2f1f58fb1b032b081deeb778e3dc21e0982d9007
joeshing <joeshing@windows10.microdone.cn>
2018-02-02 10:20:57 +0800

    初始测试

---
+++ src/main/java/xyz/timesun/imall/ImallApplication.java
@@ -0,0 +1,12 @@
+package xyz.timesun.imall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImallApplication.class, args);
+	}
+}
