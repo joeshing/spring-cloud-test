8abe0117bd8e98ee71bb89b9578ad6e291d2de78
Joeshing <joeshing@vip.qq.com>
2018-02-03 21:49:40 +0800

    增加实例

---
+++ cloud-mall/src/main/java/xyz/timesun/cloud/cloudmall/CloudMallApplication.java
@@ -0,0 +1,12 @@
+package xyz.timesun.cloud.cloudmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudMallApplication.class, args);
+	}
+}
