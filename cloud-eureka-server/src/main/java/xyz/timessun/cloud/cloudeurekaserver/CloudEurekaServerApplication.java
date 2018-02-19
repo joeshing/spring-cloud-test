e994b1370958ee69cab530914ec8906e4785b54d
Joeshing CHEN <joeshing@vip.qq.com>
2018-02-19 23:29:44 +0800

    增加config-server以及修改package的名称

--- cloud-eureka-server/src/main/java/xyz/timessun/cloud/cloudeurekaserver/CloudEurekaServerApplication.java
+++ cloud-eureka-server/src/main/java/xyz/timessun/cloud/cloudeurekaserver/CloudEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package xyz.timesun.cloud.cloudeurekaserver;
+package xyz.timessun.cloud.cloudeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
