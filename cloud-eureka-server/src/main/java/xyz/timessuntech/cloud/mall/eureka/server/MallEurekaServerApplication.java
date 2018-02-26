e2e0f0bb6c330fea7373f2cae4107dac5b118210
Joeshing CHEN <joeshing@vip.qq.com>
2018-02-26 15:48:02 +0800

    修改package

--- cloud-eureka-server/src/main/java/xyz/timessuntech/cloud/mall/eureka/server/MallEurekaServerApplication.java
+++ cloud-eureka-server/src/main/java/xyz/timessuntech/cloud/mall/eureka/server/MallEurekaServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class CloudEurekaServerApplication {
+public class MallEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudEurekaServerApplication.class, args);
+		SpringApplication.run(MallEurekaServerApplication.class, args);
 	}
 }
