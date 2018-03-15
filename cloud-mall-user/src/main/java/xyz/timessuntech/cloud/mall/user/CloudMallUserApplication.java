e87018a0ce0e0950bc95d86417f965e35fabb867
Joeshing CHEn <joeshing@vip.qq.com>
2018-03-15 17:03:00 +0800

    增加turbine-server以及turbine-dashboard 样例

--- cloud-mall-user/src/main/java/xyz/timessuntech/cloud/mall/user/CloudMallUserApplication.java
+++ cloud-mall-user/src/main/java/xyz/timessuntech/cloud/mall/user/CloudMallUserApplication.java
@@ -2,11 +2,22 @@ package xyz.timessuntech.cloud.mall.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CloudMallUserApplication {
+	
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudMallUserApplication.class, args);
