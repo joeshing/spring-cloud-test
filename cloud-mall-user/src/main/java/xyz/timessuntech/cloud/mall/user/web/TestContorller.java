1fbb104b5e9a4a44aaae2d1eb2b7f7448e3e7262
Joeshing CHEn <joeshing@vip.qq.com>
2018-03-15 17:03:00 +0800

    增加turbine-server以及turbine-dashboard 样例

--- cloud-mall-user/src/main/java/xyz/timessuntech/cloud/mall/user/web/TestContorller.java
+++ cloud-mall-user/src/main/java/xyz/timessuntech/cloud/mall/user/web/TestContorller.java
@@ -1,9 +1,12 @@
 package xyz.timessuntech.cloud.mall.user.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import xyz.timessuntech.cloud.mall.user.service.ConsumerUserService;
 
 @RefreshScope
 @RestController
@@ -11,9 +14,16 @@ public class TestContorller {
 
 	@Value("${form}")
 	private String form;
+	@Autowired
+	private ConsumerUserService userService;
 	
 	@RequestMapping("/form")
 	public String form() {
 		return this.form;
+	}
+	
+	@RequestMapping("/get")
+	public String get(String name) {
+		return userService.getUserByName(name);
 	}
 }
