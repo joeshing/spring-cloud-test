b198765c4f26ca9b8ce89517cebdaeca1ba6c0a1
Joeshing CHEn <joeshing@vip.qq.com>
2018-03-19 19:15:43 +0800

    增加amqp的配置

---
+++ cloud-mall/cloud-mall-provider/src/main/java/xyz/timessuntech/cloud/mall/service/ConsumerUserService.java
@@ -0,0 +1,49 @@
+package xyz.timessuntech.cloud.mall.service;
+
+import java.net.SocketException;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import xyz.timessuntech.cloud.cloudmall.core.bo.UserBO;
+
+@Service
+public class ConsumerUserService {
+
+	private final Logger logger = Logger.getLogger(getClass());
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@HystrixCommand(fallbackMethod = "helloFallback", commandKey = "MALL-PROVIDER")
+	public UserBO create(String name){
+		UserBO user = new UserBO(name);
+		return restTemplate.postForObject("http://CLOUD-MALL-PROVIDER/user/create", user, UserBO.class);
+	}
+
+	@HystrixCommand(fallbackMethod = "helloFallback", commandKey = "MALL-PROVIDER")
+	public UserBO get(String name) {
+		return restTemplate.getForObject("http://CLOUD-MALL-PROVIDER/user/get?name={1}", UserBO.class, name);
+	}
+
+	@HystrixCommand(fallbackMethod = "helloFallback", commandKey = "MALL-PROVIDER")
+	public boolean delete(String name){
+		try {
+			restTemplate.delete("http://CLOUD-MALL-PROVIDER/user/delete?name={1}", name);
+			return true;
+		} catch (Exception e) {
+			logger.error("delete user " + name + " error .", e);
+			return false;
+		}
+	}
+	
+	public UserBO helloFallback(String name) {
+		return null;
+	}
+}
---
+++ cloud-turbine-amqp/src/main/java/xyz/timessuntech/cloud/mall/turbine/CloudTurbineAmqpApplication.java
@@ -0,0 +1,16 @@
+package xyz.timessuntech.cloud.mall.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@EnableTurbineStream
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CloudTurbineAmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTurbineAmqpApplication.class, args);
+	}
+}
